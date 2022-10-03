bdab6a4e8c13bdaf9fdc2ff964974d668b9d8010
OlaAbaza <olaabaza020@gmail.com>
2022-10-04 00:43:20 +0200

    hide sensitive data

--- app/src/main/java/com/example/movieapp/data/remote/MovieApi.kt
+++ app/src/main/java/com/example/movieapp/data/remote/MovieApi.kt
@@ -1,9 +1,9 @@
 package com.example.movieapp.data.remote
 
-import com.example.movieapp.data.API_KEY
+import com.example.movieapp.BuildConfig
 import retrofit2.http.GET
 
 interface MovieApi {
-    @GET("now_playing?language=en-US&page=1&api_key=$API_KEY")
+    @GET("now_playing?language=en-US&page=1&api_key=${BuildConfig.API_KEY}")
     suspend fun getNowPlayingMovies(): MovieDto
 }
